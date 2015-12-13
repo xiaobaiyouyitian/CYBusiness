4a423a39234460e5be4532d874ad2f292b53d8ae
xiaobaiyouyitian <2803804184@qq.com>
2015-12-13 21:55:31 +0800

    three ok

--- etc.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#coding:utf-8
-
-import os
-
-path_tempalte = os.path.join(os.path.dirname(__file__), "templates")
-path_static = os.path.join(os.path.dirname(__file__), "static")
-cookie_secret = "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E="
